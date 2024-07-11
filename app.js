b84dcca61d746e861c82e0fd3cae81f722a0b617
Saumil Dhumal <dhumalsaumil@gmail.com>
2024-07-11 19:10:55 +0530

    Added Modules

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(3000);
---
+++ modules/user-model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/scatch");
+
+const userSchema = mongoose.Schema({
+  fullname: { type: String, minLength: 3, trim: true },
+  email: String,
+  password: String,
+  cart: {
+    typeof: Array,
+    default: [],
+  },
+  isAdmin: Boolean,
+  orders: {
+    type: Array,
+    default: [],
+  },
+  contact: Number,
+  picture: String,
+});
+
+module.exports = mongoose.model("user", userSchema);
